9e3e11a93b67864ed2babd6c5b332d36b853e5f6
Govind Maheshwari <govind@Govinds-MacBook-Air.local>
2021-05-14 23:59:53 +0530

    first commit

--- lib/investment.dart
+++ lib/investment.dart
@@ -60,6 +60,7 @@ class Return extends StatelessWidget {
           Container(
             height: 120,
             child: LineChart(LineChartData(
+              borderData: FlBorderData(show: false),
                 titlesData: FlTitlesData(
                     leftTitles: SideTitles(showTitles: false),
                     bottomTitles: SideTitles(
@@ -254,7 +255,7 @@ class RecommendedItem extends StatelessWidget {
               style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
-                  color: Colors.greenAccent),
+                  color: Colors.green),
             ),
           )
         ],
